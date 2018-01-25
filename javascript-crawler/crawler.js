5379fe78fb92965986fda555b23d46738c67d9fa
gabrielmla <gabmla19@gmail.com>
2018-01-24 21:08:29 -0300

    finalizando o crawler do partage cinema

--- javascript-crawler/crawler.js
+++ javascript-crawler/crawler.js
@@ -1,130 +1,109 @@
-var request = require('request');     // Request is used to make HTTP requests.
-var cheerio = require('cheerio');     // Cheerio is used to parse and select HTML elements on the page.
-var URL     = require('url-parse');   // URL is used to parse URLs.
+var request = require('request');
+var cheerio = require('cheerio');
 
-var START_URL = "http://partagecampina.com.br/site/filme/viva-a-vida-e-uma-festa-2/";
-var pagesVisited = {};
-var pagesToVisit = [];
+var START_URL = "http://partagecampina.com.br/site/cinema/";
+var BASE_URL  = "http://partagecampina.com.br/site/filme/";
+
+var movies = [];
+
+var linksFound = false;
 var numPagesVisited = 0;
-var url = new URL(START_URL);
-var baseUrl = url.protocol + "//" + url.hostname;
+var pagesToVisit = new Set(); // O site possui links repetidos, utilizo o Set para evitar problemas
 
-pagesToVisit.push(START_URL);
-//crawl();
+pagesToVisit.add(START_URL);
+crawl();
 
+// Realizar o crawl das paginas a serem visitadas
 function crawl() {
-  var nextPage = pagesToVisit.pop();
-  if (nextPage in pagesVisited) {
-    // We've already visited this page, so repeat the crawl
-    crawl();
-  } else {
-    // New page we haven't visited
-    visitPage(nextPage, crawl);
+  if (pagesToVisit.length <= 0) {
+    console.log(movies);
+    return movies;
   }
+
+  console.log(pagesToVisit);
+  
+  // Caso a pesquisa por links absolutos não tenha sido feita
+  if (!linksFound) {
+    nextPage = START_URL;
+  } else {
+    // Transformando o Set em Array para utilizar a função pop()
+    pagesToVisit = Array.from(pagesToVisit);
+    var nextPage = pagesToVisit.pop();
+  }
+
+  visitPage(nextPage, crawl);
 }
 
 function visitPage(url, callback) {
-  // Adicionando page para o set
-  pagesVisited[url] = true;
+  // Incrementando numero de paginas visitadas
   numPagesVisited++;
 
+  // Fazendo a request
   console.log("Visiting page " + url);
   request(url, function(error, response, body) {
-     // Verificando se o STATUS da request eh 200 (OK)
-     console.log("Status code: " + response.statusCode);
-     if(response.statusCode !== 200) {
-       callback();
-       return;
-     }
-     // Utilizando o cheerio para fazer parse do html
-     var $ = cheerio.load(body);
-     var isWordFound = searchForWord($, SEARCH_WORD);
-     if(isWordFound) {
-       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
-     } else {
-       collectInternalLinks($);
-       // In this short program, our callback is just calling crawl()
-       callback();
-     }
+    // Verificando o status HTTP (se eh igual a 200)
+    console.log("Status code: " + response.statusCode);
+    if(response.statusCode !== 200) {
+      callback();
+      return;
+    }
+    // Utilizando o cheerio para fazer o parse do HTML
+    var $ = cheerio.load(body);
+
+    // Caso seja a URL de ínicio, não adicionar
+    if (url !== START_URL) {
+      movies.push(collectMovies($));
+    }
+
+    // Na primeira iteração os links dos filmes deverão ser coletados
+    if (!linksFound) {
+      collectInternalLinks($);
+      linksFound = true;
+    }
+    // Callback para realizar um novo crawl()
+    callback();
   });
 }
 
-console.log("Visiting page " + START_URL);
-request(START_URL, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("");
-     collectMovies($);
-     //collectInternalLinks($);
-   }
-});
-
-/*function collectInternalLinks($) {
-  var allRelativeLinks = [];
-  var allAbsoluteLinks = [];
-
-  var relativeLinks = $("a[href^='/']");
-  relativeLinks.each(function() {
-      allRelativeLinks.push($(this).attr('href'));
-
-  });
-
-  var absoluteLinks = $("a[href^='http']");
-  absoluteLinks.each(function() {
-      allAbsoluteLinks.push($(this).attr('href'));
-  });
-
-  console.log("Found " + allRelativeLinks.length + " relative links");
-  console.log("Found " + allAbsoluteLinks.length + " absolute links");
-}
-
+// Função para procurar palavras no HTML
 function searchForWord($, word) {
-  var bodyText = $('html > body').text();
-  if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
-    return true;
-  }
-  return false;
-}*/
-
-function collectInternalLinks($) {
-    var absoluteLinks = $("a[href^='http']");
-    console.log("Found " + absoluteLinks.length + " relative links on page");
-    absoluteLinks.each(function() {
-        allAbsoluteLinks.push($(this).attr('href'));
-    });
+  var bodyText = $('html > body').text().toLowerCase();
+  return(bodyText.indexOf(word.toLowerCase()) !== -1);
 }
 
+// Função para coletar todos os links absolutos presentes no HTML
+function collectInternalLinks($) {
+  var absoluteLinks = $("a[href^='" + BASE_URL + "']");
+  absoluteLinks.each(function() {
+      pagesToVisit.add($(this).attr('href'));
+  });
+}
+
+// Função para coleta dos dados dos filmes do site partage
 function collectMovies($) {
   var mDetails = [];
+  var mSession = [];
   var mTitle = "Título: " + $('html > body').find('div > h1').text();
   mDetails.push(mTitle);
   var body = $('html > body').find('div.wpb_row').each(function (index) {
     $(this).find('div.wpb_wrapper').each(function (index) {
-      if ($(this).children().length == 6) {
+      if ($(this).children().length >= 5) {
         $(this).find('div').each(function (index) {
           mDetails.push($(this).text().trim());
         });
       }
     });
-    var mSession = [];
+    
     $(this).find('tr > td').each(function (index) {
       mSession.push($(this).text());
     });
-    console.log(movieJSON(mDetails, mSession));
-    //console.log(movieJSON(mDetails));
-    if (mTitle && mDetails && mSession) {
-        //var movie = {title: mTitle, description: mDescription, session: mSession};//, genre: mGenre, director: mDirector, lenght: mLenght};
-        //movies.push(movie);
-    }
   });
+  //console.log(movieJSON(mDetails, mSession));
+  var movie = movieJSON(mDetails, mSession);
+  return movie;
 }
 
+// Função para transformar os dados coletados em um JSON padronizado
 function movieJSON(details, session) {
   var json = { title: '', description: '', genre: '', oriTitle: '', director: '', duration: '', distributor: '', sessions:[] };
   var keys = Object.keys(json);
@@ -135,10 +114,13 @@ function movieJSON(details, session) {
       json[key] = details[i];
     };
   }
-
   for (i = 0; i < session.length; i += 2) {
     var aux = session[i] + " " + session[i+1];
     json[sessions].push(aux);
   }
   return json;
 }
+
+module.exports = {
+  partageCrawler: function() { return crawl() }
+};
---
+++ javascript-crawler/unique-crawler.js
@@ -0,0 +1,143 @@
+var request = require('request');     // Request is used to make HTTP requests.
+var cheerio = require('cheerio');     // Cheerio is used to parse and select HTML elements on the page.
+var URL     = require('url-parse');   // URL is used to parse URLs.
+
+var START_URL = "http://partagecampina.com.br/site/filme/gaby-estrella/";
+var pagesVisited = {};
+var pagesToVisit = [];
+var numPagesVisited = 0;
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+
+pagesToVisit.push(START_URL);
+//crawl();
+
+function crawl() {
+  var nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  } else {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
+
+function visitPage(url, callback) {
+  // Adicionando page para o set
+  pagesVisited[url] = true;
+  numPagesVisited++;
+
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Verificando se o STATUS da request eh 200 (OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Utilizando o cheerio para fazer parse do html
+     var $ = cheerio.load(body);
+     var isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
+
+console.log("Visiting page " + START_URL);
+request(START_URL, function(error, response, body) {
+   if(error) {
+     console.log("Error: " + error);
+   }
+   // Check status code (200 is HTTP OK)
+   console.log("Status code: " + response.statusCode);
+   if(response.statusCode === 200) {
+     // Parse the document body
+     var $ = cheerio.load(body);
+     console.log("");
+     collectMovies($);
+     //collectInternalLinks($);
+   }
+});
+
+/*function collectInternalLinks($) {
+  var allRelativeLinks = [];
+  var allAbsoluteLinks = [];
+
+  var relativeLinks = $("a[href^='/']");
+  relativeLinks.each(function() {
+      allRelativeLinks.push($(this).attr('href'));
+
+  });
+
+  var absoluteLinks = $("a[href^='http']");
+  absoluteLinks.each(function() {
+      allAbsoluteLinks.push($(this).attr('href'));
+  });
+
+  console.log("Found " + allRelativeLinks.length + " relative links");
+  console.log("Found " + allAbsoluteLinks.length + " absolute links");
+}
+
+function searchForWord($, word) {
+  var bodyText = $('html > body').text();
+  if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+    return true;
+  }
+  return false;
+}*/
+
+function collectInternalLinks($) {
+    var absoluteLinks = $("a[href^='http']");
+    console.log("Found " + absoluteLinks.length + " relative links on page");
+    absoluteLinks.each(function() {
+        allAbsoluteLinks.push($(this).attr('href'));
+    });
+}
+
+function collectMovies($) {
+  var mDetails = [];
+  var mSession = [];
+  var mTitle = "Título: " + $('html > body').find('div > h1.page_title').text();
+  mDetails.push(mTitle);
+  var body = $('html > body').find('div.wpb_row').each(function (index) {
+    $(this).find('div.wpb_wrapper').each(function (index) {
+      if ($(this).children().length >= 5) {
+        $(this).find('div').each(function (index) {
+          mDetails.push($(this).text().trim());
+        });
+      }
+    });
+    
+    $(this).find('tr > td').each(function (index) {
+      mSession.push($(this).text());
+    });
+  });
+
+  movie = movieJSON(mDetails, mSession);
+  console.log(movie);
+  return movie;
+}
+
+function movieJSON(details, session) {
+  var json = { title: '', description: '', genre: '', oriTitle: '', director: '', duration: '', distributor: '', sessions:[] };
+  var keys = Object.keys(json);
+  var sessions = keys.pop();
+  for (i = 0; i < details.length; i++) {
+    var key = keys[i];
+    if (json.hasOwnProperty(key)) {
+      json[key] = details[i];
+    };
+  }
+
+  for (i = 0; i < session.length; i += 2) {
+    var aux = session[i] + " " + session[i+1];
+    json[sessions].push(aux);
+  }
+  return json;
+}
